643a258fe1b11d4c6f8090e627a10b9bddd8ff30
gabriel <furstenheim@gmail.com>
2017-08-15 14:40:43 +0200

    Add schema for tweet

--- tools/README.md
+++ tools/README.md
@@ -16,4 +16,6 @@ credentials.
 
 According to Twitter usage conditions one is not allowed to permanently store tweets. So make sure to remove the table after you are done.
 
+### Benchmark
+This assumes that you already have a table tweet_benchmark with tweets. (See previous section). Run `npm run benchmark`
 
---
+++ tools/benchmark.js
@@ -0,0 +1,19 @@
+process.chdir(__dirname)
+require('dotenv').config({path: './tweets.env'})
+const pg = require('pg')
+const fs = require('fs-extra')
+main()
+async function main () {
+  const tweetSchema = fs.readJSONSync('./tweet-schema.json')
+  const client = new pg.Client()
+  await client.connect()
+  client.on('notice', function (m) {
+    console.log(m.toString())
+  })
+  // TO make sure we have latest version
+  await client.query('DROP EXTENSION IF EXISTS is_jsonb_valid')
+  await client.query('CREATE EXTENSION is_jsonb_valid')
+  const r = await client.query('SELECT id, is_jsonb_valid($1, tweet) from tweet_benchmark limit 1', [tweetSchema])
+  console.log(r.rows)
+  await client.end()
+}
